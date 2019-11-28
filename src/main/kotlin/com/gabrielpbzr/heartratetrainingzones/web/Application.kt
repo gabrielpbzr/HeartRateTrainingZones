7162220d3ffc5c3303318509c2f7a7856805d6f8
Gabriel Parreira Bezerra <gabriel.bezerra@ufv.br>
2019-11-28 09:02:35 -0300

    Adiciona classe de inicialização da aplicação Javalin

---
+++ src/main/kotlin/com/gabrielpbzr/heartratetrainingzones/web/Application.kt
@@ -0,0 +1,52 @@
+package com.gabrielpbzr.heartratetrainingzones.web
+
+import com.gabrielpbzr.heartratetrainingzones.domain.Gender
+import com.gabrielpbzr.heartratetrainingzones.usecases.CalculateHeartRateTrainingZones
+import io.javalin.Javalin;
+import io.javalin.apibuilder.ApiBuilder.*
+import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
+import java.time.LocalDate
+
+class Application {
+    fun run() {
+        val logger = LoggerFactory.getLogger("HeartTrainingZonesApp")
+        val app = Javalin.create();
+
+        app.routes {
+            get("/") { ctx -> ctx.result("Hello World") }
+
+            get("/trainingzones") { ctx ->
+                try {
+                    val useCase = CalculateHeartRateTrainingZones()
+                    val birthDate = LocalDate.parse(ctx.queryParam("birthdate"))
+                    val gender = when (ctx.queryParam("gender")) {
+                        "M" -> Gender.MALE
+                        "F" -> Gender.FEMALE
+                        else -> {
+                            throw IllegalArgumentException("Gender unknown")
+                        }
+                    }
+                    val trainingZones = useCase.calculateTrainingZonesForUser(birthDate, gender)
+                    val params = mapOf("zones" to trainingZones)
+                    //ctx.json(params);
+                    ctx.render("views/trainingzones.vm", params)
+                } catch (re : RuntimeException) {
+                    ctx.res.sendError(400, re.message)
+                } catch(e:Exception) {
+                    logger.error(e.message)
+                }
+
+            }
+
+            app.start(getDesignatedPort())
+        }
+    }
+
+    private fun getDesignatedPort(): Int {
+        if (!System.getenv("PORT").isNullOrEmpty()) {
+            return Integer.parseInt(System.getenv("PORT"))
+        }
+        return 9000
+    }
+}
