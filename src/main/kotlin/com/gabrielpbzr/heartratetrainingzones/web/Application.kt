b9379be7759038995601894b2149a7a324df3486
Gabriel Parreira Bezerra <gabriel.bezerra@ufv.br>
2019-11-29 14:40:32 -0300

    Corrige o layout das páginas

--- src/main/kotlin/com/gabrielpbzr/heartratetrainingzones/web/Application.kt
+++ src/main/kotlin/com/gabrielpbzr/heartratetrainingzones/web/Application.kt
@@ -4,8 +4,10 @@ import com.gabrielpbzr.heartratetrainingzones.domain.Gender
 import com.gabrielpbzr.heartratetrainingzones.usecases.CalculateHeartRateTrainingZones
 import io.javalin.Javalin;
 import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.http.Context
 import io.javalin.plugin.rendering.template.JavalinVelocity
 import org.apache.velocity.app.VelocityEngine
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.lang.IllegalArgumentException
 import java.time.LocalDate
@@ -13,40 +15,19 @@ import java.time.Period
 
 class Application {
     fun run() {
-        //initTemplateEngine()
-        val logger = LoggerFactory.getLogger("HeartTrainingZonesApp")
         val app = Javalin.create() {
             javalinConfig -> javalinConfig.addStaticFiles("static")
         };
 
+        val trainingZonesController = TrainingZonesController()
+        val indexController = IndexController()
+
         app.routes {
-            get("/") { ctx -> ctx.render("views/index.vm", mapOf("title" to "Início")) }
-            get("/calcular") {ctx -> ctx.render("views/calcular.vm", mapOf("title" to "Calcular", "csrfToken" to ""))}
-            post("/zonas") { ctx ->
-                try {
-                    val useCase = CalculateHeartRateTrainingZones()
-                    val birthDate = LocalDate.parse(ctx.formParam("birthdate"))
-                    val gender = when (ctx.formParam("gender")) {
-                        "M" -> Gender.MALE
-                        "F" -> Gender.FEMALE
-                        else -> {
-                            throw IllegalArgumentException("Gender unknown")
-                        }
-                    }
+            get("/", indexController::renderIndexPage)
+            get("/calcular", trainingZonesController::renderFormTrainingZones)
+            post("/zonas", trainingZonesController::getTrainingZones)
 
-                    val age = Period.between(birthDate, LocalDate.now()).years
-                    val trainingZones = useCase.calculateTrainingZonesForUser(birthDate, gender)
-                    val params = mapOf("zones" to trainingZones, "title" to "Resultados", "age" to age, "gender" to ctx.formParam("gender"))
-                    //ctx.json(params);
-                    ctx.render("views/trainingzones.vm", params)
-                } catch (re : RuntimeException) {
-                    ctx.res.sendError(400, re.message)
-                } catch(e:Exception) {
-                    logger.error(e.message)
-                }
-
-            }
-
+            app.before(this::registerComponents)
             app.start(getDesignatedPort())
         }
     }
@@ -58,10 +39,8 @@ class Application {
         return 9000
     }
 
-    private fun initTemplateEngine() {
-        val velocityEngine = VelocityEngine()
-        velocityEngine.setProperty("velocimacro.library", "src/main/resources/views/main-layout.vm")
-        velocityEngine.init()
-        JavalinVelocity.configure(velocityEngine)
+    private fun registerComponents(ctx : Context) {
+        val logger = LoggerFactory.getLogger("HeartTrainingZonesApp")
+        ctx.register(Logger::class.java, logger)
     }
 }
